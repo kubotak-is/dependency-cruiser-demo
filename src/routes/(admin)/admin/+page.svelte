23ec51963d2eb279cba10198716802b480aa1c46
kubotak <kenjiro.kubota@gmail.com>
2024-07-31 21:15:40 +0900

    modify: add page and component

---
+++ src/routes/(admin)/admin/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Title from '$lib/admin/components/Title.svelte';
+</script>
+
+<Title />
---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Title from '$lib/app/components/Title.svelte';
+</script>
+
+<Title />
